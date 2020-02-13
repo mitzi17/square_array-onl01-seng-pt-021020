1f8c42dfce736c18a49fdd2a95f4ae9a51dc811f
Mitzi Estrada <m_estrada17@live.com>
2020-02-13 07:41:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   # your code here
   array.each do |number|
-    puts number ** number
-    return number ** number = array.new
+    puts (number ** number) = array.new
+  
 end
